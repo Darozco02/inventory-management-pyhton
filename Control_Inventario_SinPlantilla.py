23dd6a09c9fea9fb33ebdec7ae913e9c85b0c7f4
Darozco02 <orozcoperezdaniel82@gmail.com>
2026-02-03 23:10:02 -0600

    Generacion PDF Sin Plantilla
    Se genero la funcion para generar un PDF sin plantilla. Todavia falta modificar las celdas para que calzen cada una.

--- Control_Inventario_SinPlantilla.py
+++ Control_Inventario_SinPlantilla.py
@@ -53,6 +53,68 @@ def calculo_Total_Inventario():
         total_inventario += i['cantidad'] * i['precio_unitario']
     print(f"💵 El precio total del inventario es: ${total_inventario} \n")
 
+# Generar Reporte
+def generar_Reporte():
+    from fpdf import FPDF
+
+    autor = input("Nombre de quien genera el reporte: ").title()
+    revisor = input("Nombre de quien revisa el reporte: ").title()
+
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("helvetica", size = 24, style="B")
+
+    # Titulo del reporte
+    pdf.cell(w = 190, h = 10, text = "Sistema de Control de Inventario", align = "C")
+    pdf.ln(20)
+    
+    resumen = """
+En seguida se muestra el inventario actual para todos los productos despues de su gestion (añadir o eliminar productos).
+Tambien se muestra el total unificado del inventario en la siguiente tabla
+"""
+
+    pdf.set_font("helvetica", size = 16 , style="B")
+    pdf.multi_cell(w = 180, h = 10, text = resumen.strip(), align = "J")
+    pdf.ln(10)
+
+    ancho_col = [60, 30, 60, 40]
+
+    # Agregar encabezados a la tabla
+    encabezado = ["Nombre", "Cantidad", "Precio Unitario", "Precio Total"]
+
+    for i, nombre_col in enumerate(encabezado):
+        pdf.set_font("helvetica", size=16 , style="B")
+        pdf.cell(w = ancho_col[i], h = 10, text= nombre_col, border=1, align="C")
+
+    pdf.ln()
+
+    # Agregar datos en las filas
+    productos = [(list(producto.values())) for producto in lista_productos]
+
+    for fila in productos:
+        for i, item in enumerate(fila):
+            if isinstance(item, float) or isinstance(item, int) and fila.index(item) != 1:
+                pdf.set_font("helvetica", size=14)
+                pdf.cell(w = ancho_col[i], h = 10, text = f"${item:.2f}", border = 1, align = "C")
+        else:
+            pdf.set_font("helvetica", size=14)
+            pdf.cell(w = ancho_col[i], h = 10, text = str(item), border = 1, align = "C")
+        pdf.ln()
+
+    #gran_total = sum(producto["valor_total"] for producto in lista_productos)
+    #pdf.set_font("helvetica", size=14 , style="B")
+    #pdf.cell(w = sum(ancho_col[:-1]), h = 10, text = f"${gran_total:.2f}", border = 1, align = "C")
+
+    pdf.ln(90)
+
+    pdf.cell(w = 80, text = f"Autor: {autor.strip().title()}", border = "T")
+    pdf.ln(30)
+    pdf.cell(w = 80, text = f"Revisor: {revisor.strip().title()}", border = "T")
+
+    # Generar archivo PDF
+    pdf.output("Reporte Sistema_de_Control_de_Inventario.pdf")
+    print("\n ✅ Archivo PDF generado con exito!!!")
+
 # Menu principal
 while True:
     print("1-Agregar Productos")
@@ -74,7 +136,7 @@ while True:
     elif opc == 4:
         calculo_Total_Inventario()
     elif opc == 5:
-        print("Este modulo todavia no esta disponible.")
+        generar_Reporte()
     elif opc == 6:
         print(f"{nombre_usuario}, Gracias por usar el sistema de control de inventario hasta luego!👋")
         break
